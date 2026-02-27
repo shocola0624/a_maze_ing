9fddae605f3819f68bb0352a691e530b75dfda79
Shotaro Kotera <sho_kotera@yahoo.co.jp>
2026-02-27 12:35:51 +0900

    maze

--- maze_utils/generate_maze.py
+++ maze_utils/generate_maze.py
@@ -1,28 +1,49 @@
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Tuple, Iterator
 from config_utils import keys as CK
 from random import seed, randint, choice
 from time import sleep
 from .print_maze import print_maze
 
 
-def build_wall_around(
-    expanded_maze: List[List[int]], x: int, y: int
+def overwrite_maze(
+    expanded_maze: List[List[int]], coord: Tuple[int], n: int
 ) -> None:
     """
     [TODO]
     """
-    for i in [-1, 0, 1]:
-        for j in [-1, 0, 1]:
-            if i != 0 or j != 0:
-                expanded_maze[y+i][x+j] = 1
+    try:
+        x, y = coord
+        expanded_maze[y][x] = n
+    except (TypeError, IndexError):
+        pass
 
 
 def initialize_maze(
-    expanded_maze: List[List[int]], exp_wd: int, exp_h: int
+    expanded_maze: List[List[int]],
+    config_data: Dict[str, Any],
+    exp_wd: int,
+    exp_h: int
 ) -> None:
     """
     [TODO]
     """
+    def build_wall_around(
+        expanded_maze: List[List[int]], x: int, y: int
+    ) -> None:
+        """
+        [TODO]
+        """
+        for i in [-1, 0, 1]:
+            for j in [-1, 0, 1]:
+                if i != 0 or j != 0:
+                    expanded_maze[y+i][x+j] = 3
+
+    # entry and exit
+    start = (i * 2 + 1 for i in config_data[CK.ENTRY])
+    goal = (i * 2 + 1 for i in config_data[CK.EXIT])
+    overwrite_maze(expanded_maze, start, 4)
+    overwrite_maze(expanded_maze, goal, 5)
+
     # wall
     for w in range(exp_wd):
         for h in range(exp_h):
@@ -155,8 +176,9 @@ def grow_wall_from(
         dir = choice(extendable_dirs)
         new_coord = lay_wall_toward(expanded_maze, coord, dir)
         wait_sec = config_data[CK.WAIT_SEC]
-        sleep(wait_sec)
-        print_maze(expanded_maze, config_data)
+        if wait_sec > 0:
+            sleep(wait_sec)
+            print_maze(expanded_maze, config_data)
         if new_coord:
             grow_wall_from(
                 expanded_maze, config_data, new_coord, taken_coords, mode
@@ -164,16 +186,16 @@ def grow_wall_from(
 
 
 def set_walls(
-        expanded_maze: List[List[int]], config_data: Dict[str, Any],
-        exp_wd: int, exp_h: int
+        expanded_maze: List[List[int]],
+        config_data: Dict[str, Any],
+        exp_wd: int,
+        exp_h: int
 ) -> None:
     """
     [TODO]壁の種をランダムな場所に植え付ける。別の関数で壁を成長させる。
     """
-    lattice_points = []
-    is_perfect = config_data[CK.PERFECT]
-
     # extend walls on 42 pattern
+    is_perfect = config_data[CK.PERFECT]
     if is_perfect:
         if exp_wd >= 15 and exp_h >= 11:
             width = config_data[CK.WIDTH]
@@ -199,6 +221,7 @@ def set_walls(
                 on_2.copy(), "to_wall"
             )
 
+    lattice_points = []
     for x in range(exp_wd):
         for y in range(exp_h):
             if x % 2 == 0 and y % 2 == 0:
@@ -211,6 +234,45 @@ def set_walls(
         grow_wall_from(expanded_maze, config_data, coord)
 
 
+def find_shortest_path(
+        expanded_maze: List[List[int]],
+        config_data: Dict[str, Any]
+) -> None:
+    """[TODO]"""
+
+    def adjacent_coord(coord: Tuple[int, int]) -> Iterator[Tuple[int, int]]:
+        """[TODO]"""
+        x, y = coord
+        around = [(x, y - 1), (x + 1, y), (x, y + 1), (x - 1, y)]
+
+        for i_x, i_y in around:
+            try:
+                if expanded_maze[i_y][i_x] in (0, 5):
+                    yield (i_x, i_y)
+            except IndexError:
+                pass
+
+    start = tuple(i * 2 + 1 for i in config_data[CK.ENTRY])
+    goal = tuple(i * 2 + 1 for i in config_data[CK.EXIT])
+    prev: Dict[Tuple[int, int], Tuple[int, int] | None] = {start: None}
+    coords = [start]
+    while coords:
+        cur = coords.pop(0)
+        if cur == goal:
+            break
+        for adj in adjacent_coord(cur):
+            if adj not in prev:
+                prev[adj] = cur
+                coords.append(adj)
+
+    cur = prev.get(goal)
+    while cur:
+        x, y = cur
+        if expanded_maze[y][x] == 0:
+            overwrite_maze(expanded_maze, cur, 6)
+        cur = prev[cur]
+
+
 def generate_expanded_maze(config_data: Dict[str, Any]) -> List[List[int]]:
     """
     [TODO]
@@ -220,16 +282,19 @@ def generate_expanded_maze(config_data: Dict[str, Any]) -> List[List[int]]:
     exp_wd = width * 2 + 1
     exp_h = height * 2 + 1
     expanded_maze = [[0 for _ in range(exp_wd)] for _ in range(exp_h)]
-    initialize_maze(expanded_maze, exp_wd, exp_h)
+    initialize_maze(expanded_maze, config_data, exp_wd, exp_h)
 
     # set seed
     try:
         seed_value = config_data[CK.SEED]
     except KeyError:
-        seed_value = randint(0, 10000)
+        seed_value = randint(0, 2147483647)
+        config_data[CK.SEED] = seed_value
     seed(seed_value)
 
     set_walls(expanded_maze, config_data, exp_wd, exp_h)
+
+    find_shortest_path(expanded_maze, config_data)
 
     return expanded_maze
 
--- maze_utils/print_maze.py
+++ maze_utils/print_maze.py
@@ -1,10 +1,11 @@
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List
 from config_utils import keys as CK
 
 
 RESET = "\x1b[0m"
 BLACK = "\x1b[40m"
 RED = "\x1b[41m"
+BLUE = "\x1b[44m"
 MAGENTA = "\x1b[45m"
 GRAY = "\x1b[47m"
 WHITE = "\x1b[107m"
@@ -17,40 +18,33 @@ def clear_screen() -> None:
     print("\x1b[2J\x1b[H", end="")
 
 
-def overwrite_maze(
-    expanded_maze: List[List[int]], coord: Tuple[int], n: int
-) -> None:
-    """
-    [TODO]
-    """
-    try:
-        x, y = coord
-        expanded_maze[y*2+1][x*2+1] = n
-    except (TypeError, IndexError):
-        pass
-
-
 def print_maze(
-        expanded_maze: List[List[int]], config_data: Dict[str, Any]
+        expanded_maze: List[List[int]],
+        config_data: Dict[str, Any],
+        color_scheme: List[str] | None = None,
+        show_path: bool | None = False
 ) -> None:
     """
     [TODO]
     """
-    entry = config_data[CK.ENTRY]
-    exit = config_data[CK.EXIT]
-    overwrite_maze(expanded_maze, entry, 3)
-    overwrite_maze(expanded_maze, exit, 4)
-
-    # black floor, white wall, magenta entry, red exit
+    # 0 floor, 1 wall, 2 inside 42, 3 outside 42, 4 entry, 5 exit, 6 path
+    if color_scheme is None:
+        color_scheme = [BLACK, WHITE, GRAY, WHITE, MAGENTA, RED, BLUE]
     colors = {
-        0: BLACK,
-        1: WHITE,
-        2: GRAY,
-        3: MAGENTA,
-        4: RED
+        0: color_scheme[0],
+        1: color_scheme[1],
+        2: color_scheme[2],
+        3: color_scheme[3],
+        4: color_scheme[4],
+        5: color_scheme[5]
     }
+    if show_path:
+        colors.update({6: color_scheme[6]})
+    else:
+        colors.update({6: color_scheme[0]})
     str_maze = ""
 
+    # print maze
     clear_screen()
     for i in expanded_maze:
         for j in i:
@@ -60,17 +54,19 @@ def print_maze(
                 str_maze += "  "
         str_maze += "\n"
     print(str_maze)
+    print("seed:", config_data[CK.SEED])
 
 
 if __name__ == "__main__":
-    from maze_utils import generate_expanded_maze
+    from maze_utils import generate_expanded_maze, ask_next_process
     config_data = {
         CK.WIDTH: 30,
         CK.HEIGHT: 30,
         CK.ENTRY: (0, 0),
         CK.EXIT: (29, 29),
         CK.PERFECT: True,
-        CK.WAIT_SEC: 0.01
+        CK.WAIT_SEC: 0.0
     }
     expanded_maze = generate_expanded_maze(config_data)
     print_maze(expanded_maze, config_data)
+    ask_next_process(expanded_maze, config_data, "")
