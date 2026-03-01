d85616175a2a8fc4e8fd6f7264680b1ce90203d7
Shotaro Kotera <sho_kotera@yahoo.co.jp>
2026-03-01 18:42:10 +0900

    output_maze, add entry/exit coords and shortest path from entry

--- maze_utils/output_maze.py
+++ maze_utils/output_maze.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import Any, Dict, List, Optional
+from config_utils import keys as CK
 
 
 def int_to_hex(num: int) -> str:
@@ -14,19 +15,34 @@ def int_to_hex(num: int) -> str:
     return f"{num:X}"
 
 
-def output_maze(maze: List[List[int]], output_path: str) -> None:
+def output_maze(
+    maze: List[List[int]],
+    output_path: str,
+    config_data: Dict[str, Any],
+    shortest_path: Optional[str]
+) -> None:
     """
     Writes a 2D maze to a text file in hexadecimal format.
+
     Args:
         maze: A 2D list of integers representing the maze.
         output_path: The file path where the maze will be saved.
     """
+    output_str = ""
+    start = config_data[CK.ENTRY]
+    goal = config_data[CK.EXIT]
     with open(output_path, "w") as f:
         for row in maze:
             line_str = ""
             for cell in row:
                 line_str += int_to_hex(cell)
-            f.write(line_str + "\n")
+            output_str += line_str + "\n"
+        output_str += "\n"
+        output_str += str(start)[1:-1].replace(" ", "") + "\n"
+        output_str += str(goal)[1:-1].replace(" ", "") + "\n"
+        if shortest_path:
+            output_str += shortest_path + "\n"
+        f.write(output_str)
 
 
 if __name__ == "__main__":
